774437f73b88b400fa1a5c1cb802b78ca95d066a
Hari Srinivasulu <srinivasulu.hari@gmail.com>
2020-09-08 14:35:04 +0530

    Added a test endpoint

--- server.js
+++ server.js
@@ -2,4 +2,10 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 
+app.use(express.json())
+
+app.get('/api/tasks', (req, res) => {
+    res.send('<html><body><h1>God save the queen</h1></body></html>')
+})
+
 app.listen(process.env.PORT, () => {console.log(`Listening at port ${process.env.PORT}`)})
