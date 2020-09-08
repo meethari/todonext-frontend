503cfb06bdf69bae3d6948396a87f2ec6bc720b0
Hari Srinivasulu <srinivasulu.hari@gmail.com>
2020-09-08 16:31:30 +0530

    Added GET method to api/tasks endpoint

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
+// Requires
 const express = require('express')
 require('dotenv').config()
+const mongoose = require('mongoose')
+
+const setUpMongoose = () => {
+    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    var taskSchema = mongoose.Schema({
+        text: String,
+        done: Boolean
+    })
+    var Task = mongoose.model('task', taskSchema)
+    return Task
+}
+
 const app = express()
+
+var Task = setUpMongoose()
 
 app.use(express.json())
 
-app.get('/api/tasks', (req, res) => {
-    res.send('<html><body><h1>God save the queen</h1></body></html>')
+app.get('/api/tasks', async (req, res) => {
+    const taskList = await Task.find()
+    res.send(taskList)
 })
 
 app.listen(process.env.PORT, () => {console.log(`Listening at port ${process.env.PORT}`)})
