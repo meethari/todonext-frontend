db9098bed49dec567b6bd252dbcd4ae0dabb4888
Hari Srinivasulu <srinivasulu.hari@gmail.com>
2021-01-31 18:37:10 +0530

    Added heroku-postbuild script and code to serve static client

--- server.js
+++ server.js
@@ -105,4 +105,14 @@ app.delete('/api/tasks/:id', async (req, res) => {
     }
 })
 
+if (process.env.NODE_ENV == 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
+
 app.listen(process.env.PORT, () => {console.log(`Listening at port ${process.env.PORT}`)})
