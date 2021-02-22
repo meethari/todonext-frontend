58ba212fde56ec0550f405287eec3adfd827d34c
Hari Srinivasulu <srinivasulu.hari@gmail.com>
2021-02-22 13:41:54 +0530

    Register page sends POST requests, AuthContext set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Splash from 'pages/Splash'
 import TodoApp from 'pages/TodoApp'
 import Login from 'pages/Login'
 import Register from 'pages/Register'
+import {AuthContext} from 'context/Auth'
 import 'App.css';
 
 
-const App = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/">
-        <Splash/>
-      </Route> 
-      <Route path="/app">
-        <TodoApp/>
-      </Route>
-      <Route path="/login">
-        <Login/>
-      </Route>
-      <Route path="/register">
-        <Register/>
-      </Route>
-    </Switch>
-  </Router>
-  
-)
+const App = () => {
+  const existingTokens = JSON.parse(localStorage.getItem("tokens"));
+  const [authTokens, setAuthTokens] = useState(existingTokens);
+
+  const setTokens = (data) => {
+    localStorage.setItem("tokens", JSON.stringify(data));
+    setAuthTokens(data);
+  }
+
+  return (
+    <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Splash/>
+          </Route> 
+          <Route path="/app">
+            <TodoApp/>
+          </Route>
+          <Route path="/login">
+            <Login/>
+          </Route>
+          <Route path="/register">
+            <Register/>
+          </Route>
+        </Switch>
+      </Router>
+    </AuthContext.Provider>
+  )
+}
 
 
 export default App;
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,36 +1,59 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import axios from 'axios'
 import { Button, Form, FormGroup, Label, Input, FormText} from 'reactstrap'
 import { Link } from 'react-router-dom'
 
-const Register = () => (
-    <> 
-        <div className="register__title">
-            Register
-        </div>
-        <div className="register__container">
-            <Form className="register__form">
-                <FormGroup>
-                    <Label form="inputEmail">Email ID</Label>
-                    <Input type="email" name="inputEmail"/>
-                </FormGroup>
-                <FormGroup>
-                    <Label form="inputPassword">Password</Label>
-                    <Input type="password" name="inputPassword"/>
-                </FormGroup>
-                <FormGroup>
-                    <Label form="inputPasswordRepeat">Password (again)</Label>
-                    <Input type="password" name="inputPasswordRepeat"/>
-                </FormGroup>
-                <div style={{textAlign: 'right'}}><Button color="primary">Sign up</Button></div>
-                <br/>
-                <FormText color="muted">
-                Already have an account?
-                Click <Link to="/login">here</Link> to sign in.
-                </FormText>
-            </Form>
-        </div>
-    </>
-)
+const Register = () => {
 
+    const [inputEmail, setInputEmail] = useState("")
+    const [inputPassword, setInputPassword] = useState("")
+    const [inputPasswordRepeat, setInputPasswordRepeat] = useState("")
+
+    const formValidate = () => {
+        if (inputPassword.localeCompare(inputPasswordRepeat) != 0) {
+            return false
+        }
+        
+        return true
+    }
+
+    const submitForm = async () => {
+        console.log(inputEmail, inputPassword, inputPasswordRepeat)
+
+        if (!formValidate())
+            return
+        
+        const response = await axios.post('/register', {username: inputEmail, password: inputPassword})
+    }
+
+    return (
+        <> 
+            <div className="register__title">
+                Register
+            </div>
+            <div className="register__container">
+                <Form className="register__form">
+                    <FormGroup>
+                        <Label form="inputEmail">Email ID</Label>
+                        <Input type="email" name="inputEmail" value={inputEmail} onChange={(e) => {setInputEmail(e.target.value)}}/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label form="inputPassword">Password</Label>
+                        <Input type="password" name="inputPassword" value={inputPassword} onChange={(e) => {setInputPassword(e.target.value)}}/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label form="inputPasswordRepeat">Password (again)</Label>
+                        <Input type="password" name="inputPasswordRepeat" value={inputPasswordRepeat} onChange={(e) => {setInputPasswordRepeat(e.target.value)}}/>
+                    </FormGroup>
+                    <div style={{textAlign: 'right'}}><Button color="primary" onClick={submitForm}>Sign up</Button></div>
+                    <br/>
+                    <FormText color="muted">
+                    Already have an account?
+                    Click <Link to="/login">here</Link> to sign in.
+                    </FormText>
+                </Form>
+            </div>
+        </>
+    )
+}   
 export default Register
